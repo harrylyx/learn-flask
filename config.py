51c6a22f768e31649960e829d1935d09a9964da5
harrylyx <lyxvae@gmail.com>
2018-08-03 14:17:45 +0800

    第九章 分页

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['harrylyx@qq.com']
+    POSTS_PER_PAGE = 25
