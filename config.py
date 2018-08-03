e24ca4a5c7533ca1d372fe8af89567055647a191
harrylyx <lyxvae@gmail.com>
2018-08-03 15:23:47 +0800

    第十章 利用邮件进行重置密码

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or basedir
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['harrylyx@qq.com']
+    MAIL_USERNAME = 'your-email@163.com'
+    MAIL_PASSWORD = "your-password"
+    ADMINS = ['your-email@163.com']
     POSTS_PER_PAGE = 25
